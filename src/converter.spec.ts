4f42550fd11779a28c10979ca608090e08e43fbb
Aime Pelozo <aime.pelozo@celeri.app>
2022-09-20 08:52:38 -0300

    refactor kata

--- src/converter.spec.ts
+++ src/converter.spec.ts
@@ -1,92 +1,97 @@
-import converter from './converter';
-describe('Converter', () => {
-    test('should return I for 1', () => {
-        console.log(converter(1));
-        expect(converter(1)).toBe("I");
-    });
-    test('should return V for 5', () => {
-        expect(converter(5)).toBe("V");
-    });
-    test('should return X for 10', () => {
-        expect(converter(10)).toBe("X");
-    });
-    test('should return L for 50', () => {
-        expect(converter(50)).toBe("L");
-    });
-    test('should return C for 100', () => {
-        expect(converter(100)).toBe("C");
-    });
-    test('should return D for 500', () => {
-        expect(converter(500)).toBe("D");
-    });
-    test('should return M for 1000', () => {
-        expect(converter(1000)).toBe("M");
-    });
-    test('should return IV for 4', () => {
-        expect(converter(4)).toBe("IV");
-    });
-    test('should return IX for 9', () => {
-        expect(converter(9)).toBe("IX");
-    });
-    test('should return XL for 40', () => {
-        expect(converter(40)).toBe("XL");
-    });
-    test('should return XC for 90', () => {
-        expect(converter(90)).toBe("XC");
-    });
-    test('should return CD for 400', () => {
-        expect(converter(400)).toBe("CD");
-    });
-    test('should return CM for 900', () => {
-        expect(converter(900)).toBe("CM");
-    });
-    test('should return III for 3', () => {
-        expect(converter(3)).toBe("III");
-    });
-    test('should return VIII for 8', () => {
-        expect(converter(8)).toBe("VIII");
-    });
-    test('should return XXX for 30', () => {
-        expect(converter(30)).toBe("XXX");
-    });
-    test('should return LXXX for 80', () => {
-        expect(converter(80)).toBe("LXXX");
-    });
-    test('should return CCC for 300', () => {
-        expect(converter(300)).toBe("CCC");
-    });
-    test('should return DCCC for 800', () => {
-        expect(converter(800)).toBe("DCCC");
-    });
-    test('should return MMM for 3000', () => {
-        expect(converter(3000)).toBe("MMM");
-    });
-    test('should return MMMCMXCIX for 3999', () => {
-        expect(converter(3999)).toBe("MMMCMXCIX");
-    });
-    test('should return CCCXCIX for 399', () => {
-        expect(converter(399)).toBe("CCCXCIX");
-    });
-    test('should return CCCLI for 351', () => {
-        expect(converter(351)).toBe("CCCLI");
-    });
-    test('should return MCCXCIX for 1299', () => {
-        expect(converter(1299)).toBe("MCCXCIX");
-    });
-    test('should return MDCCXXI for 1721', () => {
-        expect(converter(1721)).toBe("MDCCXXI");
-    });
-    test('should return MI for 1001', () => {
-        expect(converter(1001)).toBe("MI");
-    });
-    test('should return CDLXXIX for 479', () => {
-        expect(converter(479)).toBe("CDLXXIX");
-    });
-    test('should return MCMXCIX for 1999', () => {
-        expect(converter(1999)).toBe("MCMXCIX");
-    });
-    test('should return MMMDCCCLVIII for 3858', () => {
-        expect(converter(3858)).toBe("MMMDCCCLVIII");
-    });
-
-});
+import converter from "./converter"
+describe("Converter", () => {
+    test("should return I for 1", () => {
+        console.log(converter(1))
+        expect(converter(1)).toBe("I")
+    })
+    test("should return V for 5", () => {
+        expect(converter(5)).toBe("V")
+    })
+    test("should return X for 10", () => {
+        expect(converter(10)).toBe("X")
+    })
+    test("should return L for 50", () => {
+        expect(converter(50)).toBe("L")
+    })
+    test("should return C for 100", () => {
+        expect(converter(100)).toBe("C")
+    })
+    test("should return D for 500", () => {
+        expect(converter(500)).toBe("D")
+    })
+    test("should return M for 1000", () => {
+        expect(converter(1000)).toBe("M")
+    })
+    test("should return IV for 4", () => {
+        expect(converter(4)).toBe("IV")
+    })
+    test("should return IX for 9", () => {
+        expect(converter(9)).toBe("IX")
+    })
+    test("should return XL for 40", () => {
+        expect(converter(40)).toBe("XL")
+    })
+    test("should return XC for 90", () => {
+        expect(converter(90)).toBe("XC")
+    })
+    test("should return CD for 400", () => {
+        expect(converter(400)).toBe("CD")
+    })
+    test("should return CM for 900", () => {
+        expect(converter(900)).toBe("CM")
+    })
+    test("should return III for 3", () => {
+        expect(converter(3)).toBe("III")
+    })
+    test("should return VIII for 8", () => {
+        expect(converter(8)).toBe("VIII")
+    })
+    test("should return XXX for 30", () => {
+        expect(converter(30)).toBe("XXX")
+    })
+    test("should return LXXX for 80", () => {
+        expect(converter(80)).toBe("LXXX")
+    })
+    test("should return CCC for 300", () => {
+        expect(converter(300)).toBe("CCC")
+    })
+    test("should return DCCC for 800", () => {
+        expect(converter(800)).toBe("DCCC")
+    })
+    test("should return MMM for 3000", () => {
+        expect(converter(3000)).toBe("MMM")
+    })
+    test("should return MMMCMXCIX for 3999", () => {
+        expect(converter(3999)).toBe("MMMCMXCIX")
+    })
+    test("should return CCCXCIX for 399", () => {
+        expect(converter(399)).toBe("CCCXCIX")
+    })
+    test("should return CCCLI for 351", () => {
+        expect(converter(351)).toBe("CCCLI")
+    })
+    test("should return MCCXCIX for 1299", () => {
+        expect(converter(1299)).toBe("MCCXCIX")
+    })
+    test("should return MDCCXXI for 1721", () => {
+        expect(converter(1721)).toBe("MDCCXXI")
+    })
+    test("should return MI for 1001", () => {
+        expect(converter(1001)).toBe("MI")
+    })
+    test("should return CDLXXIX for 479", () => {
+        expect(converter(479)).toBe("CDLXXIX")
+    })
+    test("should return MCMXCIX for 1999", () => {
+        expect(converter(1999)).toBe("MCMXCIX")
+    })
+    test("should return MMMDCCCLVIII for 3858", () => {
+        expect(converter(3858)).toBe("MMMDCCCLVIII")
+    })
+    test("should return NONE for 0", () => {
+        expect(converter(0)).toBe("NONE")
+    })
+    test("should return XI for 11", () => {
+        expect(converter(11)).toBe("XI")
+    })
+})
--- src/converter.ts
+++ src/converter.ts
@@ -1,27 +1,44 @@
-let conversion: any = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1
+const ROMAN_TABLE: any = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    9: "IX",
+    10: "X",
+    40: "XL",
+    50: "L",
+    90: "XC",
+    100:"C",
+    400: "CD",
+    500: "D",
+    900: "CM",
+    1000: "M"
 }
 
-const converter = (arabicNumber: number): void => {
-    var romano = ""
-    for (let i  in conversion) {
-        while (arabicNumber >= conversion[i]) {
-            romano += 1
-            arabicNumber -= conversion[i]
-        }
+const converter = (arabicNumber: number): string => {
+    //el resultado es un string vacio porque aca cargamos la data final
+ let result : string = "";
+ //tomo la key del object y la invertimos para que vaya buscando el numero de mayor a menor y vaya bajando en caso de no cumplir
+ const ROMAN_TABLE_KEY = Object.keys(ROMAN_TABLE).reverse();
+//entra la key para recorrer el for each
+ ROMAN_TABLE_KEY.forEach(value => {
+    //pasa la key a un number pqe llega en string
+    const key = Number(value);
+    //cuando nuestro numero arabico sea mayor igual que la key
+    while (arabicNumber >= key) {
+        //al numero arabico le restamos la key
+        arabicNumber -= key;
+        //el resultado es el valor de la key que se encuentra en la tabla romana
+        result += ROMAN_TABLE[key];
     }
-    return romano as void
-}
-export default converter
+ })
+ return result
+};
+export default converter;
+
+/* 
+while(arabicNumber > 0){
+    if(arabicNumber >= 40){
+        result = result + ROMAN_TABLE[40]
+    }
+} */
+
